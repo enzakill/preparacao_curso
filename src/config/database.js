4940851982adbcabbdae397830bab7142cd35d2e
Jonathan Vinicius Braz Silva <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-10-16 23:32:10 -0300

    Update servidor

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    // host: 'mysql669.umbler.com',
+    // database: 'linefast',
+    // username: 'jonathanfast',
+    // password: '78951fla',
+    // dialect: 'mysql',
+    // define: {
+    //     timestamps: 'true'
+    // }
+  
+
+    host: 'localhost',
+    database: 'preparacao_curso',
+    username: 'root',
+    password: '',
+    dialect: 'mysql',
+    define: {
+        timestamps: true,
+    }
+}
